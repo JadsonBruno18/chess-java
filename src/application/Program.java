bee09b690edd191b964442ad8abd4c1f3de523fc
Santos <jadson.santos@capgemini.com>
2025-09-02 12:23:26 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,16 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Olá mundo!");
+		
+	Position position = new Position(2, 1); 
+	
+	System.out.println(position);
+		
+		
 
 	}
 
