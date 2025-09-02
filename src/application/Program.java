c31b70b1a9fd0e28bf827da441feba975503e306
Santos <jadson.santos@capgemini.com>
2025-09-02 12:49:54 -0300

    Starting to implement Board and Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,14 @@
 package application;
 
-import boardgame.Position;
+import boardgame.Board;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-	Position position = new Position(2, 1); 
+	Board board = new Board(1,2);
 	
-	System.out.println(position);
-		
+	System.out.println(board);
 		
 
 	}
